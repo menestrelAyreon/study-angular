dd0bf2d016c424be82631889b150f6b5034d1b8b
menestrelAyreon <menetrelAyreon@hotmail.com>
2019-03-24 23:46:23 +0000

    add car class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ListCarsComponent } from './components/list-cars/list-cars.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ListCarsComponent
+    
   ],
   imports: [
     BrowserModule,
